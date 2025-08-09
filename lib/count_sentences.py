d280fce5ce73ca40f90c2fc6016a33f61306462a
mohammedhassa15 <mohammedhassan0041@gmail.com>
2025-08-09 20:58:13 +0300

    fix string class

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,33 @@
-#!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self._value = None
+        self.value = value  # Uses setter
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, val):
+        if isinstance(val, str):
+            self._value = val
+        else:
+            print("The value must be a string.")
+
+    def is_sentence(self):
+        return isinstance(self.value, str) and self.value.endswith(".")
+
+    def is_question(self):
+        return isinstance(self.value, str) and self.value.endswith("?")
+
+    def is_exclamation(self):
+        return isinstance(self.value, str) and self.value.endswith("!")
+
+    def count_sentences(self):
+        if not self.value or not isinstance(self.value, str):
+            return 0
+        sentences = re.split(r'[.!?]+', self.value.strip())
+        sentences = [s for s in sentences if s.strip()]
+        return len(sentences)
